dcea981c359624a44564933fa8aab19abd7877f7
rfrenchy <ryanfrench@outlook.com>
2025-01-08 12:19:59 +0000

    wave projectile back and forth;

--- src/characters/WaveProjectile.ts
+++ src/characters/WaveProjectile.ts
@@ -25,46 +25,37 @@ export class WaveProjectile extends GameObjects.Sprite {
         this.x_direction = Phaser.Math.Vector2.RIGHT
         this.speed = Phaser.Math.GetSpeed(200, 1)
 
-        const seg = (Math.PI * 2) / 360
-        const scale = 2
+        this.yPath();
+    }
+
+    private yPath() {
+        const seg = (Math.PI * 2) / 360;
+        const scale = 2;
         for (let i = 0; i < 360; i++) {
-            this.y_path[i] = Math.sin(seg * i) * scale
+            this.y_path[i] = Math.sin(seg * i) * scale;
         }
-
-        // add to phaser physics engine
-        // take y coords from a rotation
-
-        // pre calc y?
-
-
-
-        // assume y 0 on an edge of a circle, 
-        // simulate rotation around repeatedly
-        
-        // def rotation(theta):
-        // return np.array([[ np.cos(theta), -np.sin(theta)   ],
-        //                  [ np.sin(theta),  np.cos(theta)   ]])
-     
-        
     }
 
     start() {
         
     }
 
-    update (time: number, delta: number) {
+    public update (time: number, delta: number) {
         if (this.active) {
+            // horizontal movement
             this.x += this.x_direction.x * this.speed * delta
-            // console.log(time % 360)
-            console.log(this.y)
+
+            // vertical movement
             this.y += this.y_path[this.y_index % 360] 
             this.y_index++
         }
 
-        if (this.x > this.scene.sys.canvas.width || this.x < 0 ) {
-            this.setActive(false);
-            this.setVisible(false);
-            this.destroy();
+        if (this.x > this.scene.sys.canvas.width) {
+            this.x_direction = Phaser.Math.Vector2.LEFT
+        }
+
+        if (this.x < 0) {
+            this.x_direction = Phaser.Math.Vector2.RIGHT
         }
     }
 
